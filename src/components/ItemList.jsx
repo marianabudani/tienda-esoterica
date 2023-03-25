a97e4c59653080438c46d3a5eb5bce26d50af4a8
Mariana Budani <mariana.budani@gmail.com>
2023-03-25 18:41:18 -0300

    correccion

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,20 +1,22 @@
 import Item from './Item'
 import { Container } from '@chakra-ui/react'
+import Products from '../products.json'
 
-const ItemList = ({ list }) => {
+const ItemList = () => {
+  const list = Products.products
   return (
     <>
     <Container maxW='max-content' alignItems='center'>
       {list.map((product) => (
         <Item
-        key={product.id}
-        id={product.id}
-        name={product.name}
-        image={product.image}
-        description={product.description}
-        price={product.price}
-        stock={product.stock}
-        category={product.category}
+          key={product.id}
+          id={product.id}
+          name={product.name}
+          image={product.image}
+          description={product.description}
+          price={product.price}
+          stock={product.stock}
+          category={product.category}
         />
       ))}
     </Container>
